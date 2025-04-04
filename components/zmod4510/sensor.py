50444cafbed8e36503a4344bfd3ad64654762187
Z3r0shin <z3r0shin@gmail.com>
2025-04-04 15:32:33 -0400

    modified:   components/zmod4510/sensor.py

--- components/zmod4510/sensor.py
+++ components/zmod4510/sensor.py
@@ -1,4 +1,1 @@
-# Minimal sensor file for the ZMOD4510 component.
-# (Additional sensor-specific configuration or helper functions could go here.)
-import esphome.config_validation as cv
-
+from . import *
