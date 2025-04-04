59051a9ee2ed51cc21b536463ea6efdf9c980218
Zeroshin <z3r0shin@gmail.com>
2025-04-04 13:19:27 -0400

    Update zmod4510_component.h

--- components/zmod4510/zmod4510_component.h
+++ components/zmod4510/zmod4510_component.h
@@ -1,7 +1,6 @@
 #pragma once
 
-#include "esphome/core/component.h"
-#include "esphome/components/polling_component/polling_component.h"
+#include "esphome/core/component.h"        // This header defines Component and PollingComponent.
 #include "esphome/components/i2c/i2c.h"
 #include "esphome/components/sensor/sensor.h"
 #include <cstring>  // For memcpy
@@ -27,7 +26,6 @@ class ZMOD4510 : public esphome::PollingComponent, public esphome::i2c::I2CDevic
   void set_o3_sensor(esphome::sensor::Sensor *sensor);
   void set_aqi_sensor(esphome::sensor::Sensor *sensor);
 
-  // Component lifecycle functions
   void setup() override;
   void update() override;
 
@@ -37,17 +35,14 @@ class ZMOD4510 : public esphome::PollingComponent, public esphome::i2c::I2CDevic
   esphome::sensor::Sensor *o3_sensor_{nullptr};
   esphome::sensor::Sensor *aqi_sensor_{nullptr};
 
-  // Device structure and algorithm handle (from Renesas library)
+  // Renesas device structure and algorithm state.
   zmod4xxx_dev_t dev_;
   no2_o3_handle_t algo_handle_;
 
-  // Buffers for configuration and ADC results.
-  // - "config_" will hold the 6-byte configuration read from the sensor.
-  // - "prod_data_" holds production data (length defined in the configuration).
-  // - "adc_buffer_" holds the ADC result from the sensor (length is from meas_conf->r.len).
+  // Buffers for configuration and ADC measurement data.
   uint8_t config_[6];
-  uint8_t prod_data_[ZMOD4510_PROD_DATA_LEN];  // ZMOD4510_PROD_DATA_LEN is defined in the config header (10 bytes)
-  uint8_t adc_buffer_[32];                     // Measurement configuration "r" length is 32 bytes.
+  uint8_t prod_data_[ZMOD4510_PROD_DATA_LEN];  // ZMOD4510_PROD_DATA_LEN from config header
+  uint8_t adc_buffer_[32];                     // According to measurement configuration length
 };
 
 }  // namespace zmod4510
